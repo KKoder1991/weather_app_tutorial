870442903b2d1765d2a09f7f004df3dc336ec9f4
KKoder1991 <konondlovu@icloud.com>
2022-03-16 18:28:56 +0100

    Add error handler to the Home component

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React from "react";
 import { useLazyQuery } from "@apollo/client";
 import { GET_WEATHER_QUERY } from "./graphql/Queries";
 
-function Home(){
-    return (
-        <div className="home">
-            <h2>Search the Weather</h2>
-            <input type="text" placeholder="City Name"/>
-            <button>Search</button>
-        </div>
-    );
-};
+function Home() {
+  const [getWeather, { loading, data, error }] = useLazyQuery(GET_WEATHER_QUERY, {
+    variables: { name: "Vancouver" },
+  });
 
+  if (error) return <h1>Error found</h1>
+
+  return (
+    <div className="home">
+      <h2>Search the Weather</h2>
+      <input type="text" placeholder="City Name" />
+      <button onClick={() => getWeather()}>Search</button>
+    </div>
+  );
+}
 
 export default Home;
