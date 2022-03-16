c15af5b252fd459decce8c7cbf6759968b883add
KKoder1991 <konondlovu@icloud.com>
2022-03-16 18:04:45 +0100

    Create new client for recieving weather data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import "./App.css";
 import Home from "./Pages/Home";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
 
-function App(){
+function App() {
+
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: "https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbmxwLVhyem1ReHlVSld0WW04eXVPVUNyWlVpZ3xBQ3Jtc0ttWFFRbWI1YllYOEFRQXRHdERCU3B6bF9hTnlIbHlHdU9WRFBpMjZZU0t2VmpBbHRtNEtuamFudHNidDU2cHlKYy04VDQ2VGxkUkM3ZE5seGh3YXdlN2l0SGRHNXFpMEJVcUtuTEpFQUhnR2p3eWl5Zw&q=https%3A%2F%2Fgraphql-weather-api.herokuapp.com%2F"
+  })
   return (
-    <div className="App">
+    <ApolloProvider client={client}>
       <Home />
-    </div>
+    </ApolloProvider>
   );
-};
+}
 
 export default App;
