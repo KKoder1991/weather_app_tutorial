e93b1dae479976ab8b9000ac4b11eb617cdcfb30
KKoder1991 <konondlovu@icloud.com>
2022-03-16 18:45:55 +0100

    Add setters and getters for city search in input area

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 
   const client = new ApolloClient({
     cache: new InMemoryCache(),
-    uri: "https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbmxwLVhyem1ReHlVSld0WW04eXVPVUNyWlVpZ3xBQ3Jtc0ttWFFRbWI1YllYOEFRQXRHdERCU3B6bF9hTnlIbHlHdU9WRFBpMjZZU0t2VmpBbHRtNEtuamFudHNidDU2cHlKYy04VDQ2VGxkUkM3ZE5seGh3YXdlN2l0SGRHNXFpMEJVcUtuTEpFQUhnR2p3eWl5Zw&q=https%3A%2F%2Fgraphql-weather-api.herokuapp.com%2F"
+    uri: "https://graphql-weather-api.herokuapp.com"
   })
   return (
     <ApolloProvider client={client}>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLazyQuery } from "@apollo/client";
-import { GET_WEATHER_QUERY } from "./graphql/Queries";
+import { GET_WEATHER_QUERY } from "../graphql/Queries";
 
 function Home() {
-  const [getWeather, { loading, data, error }] = useLazyQuery(GET_WEATHER_QUERY, {
-    variables: { name: "Vancouver" },
-  });
+  const [citySearched, setCitySearched] = useState("");
+  const [getWeather, { loading, data, error }] = useLazyQuery(
+    GET_WEATHER_QUERY,
+    {
+      variables: { name: "Vancouver" },
+    }
+  );
 
-  if (error) return <h1>Error found</h1>
+  if (error) return <h1>Error found</h1>;
+  if (data) {
+    console.log(data);
+  }
 
   return (
     <div className="home">
       <h2>Search the Weather</h2>
-      <input type="text" placeholder="City Name" />
+      <input
+        type="text"
+        placeholder="City Name"
+        onChange={(event) => {
+          setCitySearched(event.target.value);
+        }}
+      />
       <button onClick={() => getWeather()}>Search</button>
     </div>
   );
