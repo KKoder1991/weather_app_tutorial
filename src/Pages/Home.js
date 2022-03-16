56b7d1c7d90473f07c3094648ce17ea036139b41
KKoder1991 <konondlovu@icloud.com>
2022-03-16 17:57:21 +0100

    Add components to home page for home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 function Home(){
-    <div className=""></div>
+    return (
+        <div className="home">
+            <h2>Search the Weather</h2>
+            <input type="text" placeholder="City Name"/>
+            <button>Search</button>
+        </div>
+    );
 };
 
 
